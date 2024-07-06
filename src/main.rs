38f12195001e56064fe4c74854259e3138449a11
Steve <sfrench83@gmail.com>
2024-07-06 12:45:16 +0800

    Compiles

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,10 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
 use cortex_m::asm;
 use cortex_m_rt::entry;
 
+// allow unused import to compile and link
+#[allow(unused_imports)]
+use stm32f0xx_hal as hal;
+
 #[entry]
 fn main() -> ! {
     asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
