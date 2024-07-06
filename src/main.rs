540e2ae0b021b95a03d6ebc1e82d7ff161ec590b
SDEEV <=>
2024-07-06 15:11:27 +0800

    get led blinking

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,35 @@
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
-use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// use panic_abort as _; // requires nightly
-// use panic_itm as _; // logs messages over ITM; requires ITM support
-// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
-
 use cortex_m::asm;
+use cortex_m::interrupt::free;
 use cortex_m_rt::entry;
-
-// allow unused import to compile and link
-#[allow(unused_imports)]
+use hal::delay::Delay;
+use hal::prelude::*;
+use hal::stm32;
 use stm32f0xx_hal as hal;
+use stm32f0xx_hal::pac::ITM;
+// pick a panicking behavior
+use cortex_m::iprintln;
+use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+                     // use panic_abort as _; // requires nightly
+                     // use panic_itm as _; // logs messages over ITM; requires ITM support
+                     // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    let mut p = stm32::Peripherals::take().unwrap();
+    let mut cp = cortex_m::Peripherals::take().unwrap();
 
+    let mut rcc = p.RCC.configure().sysclk(48.mhz()).freeze(&mut p.FLASH);
+
+    let s = p.GPIOA.split(&mut rcc);
+
+    let mut led = free(|cs| s.pa5.into_push_pull_output(cs));
+
+    let mut delay = Delay::new(cp.SYST, &rcc);
     loop {
-        // your code goes here
+        delay.delay_ms(1_000_u16);
+        led.toggle().unwrap();
     }
 }
